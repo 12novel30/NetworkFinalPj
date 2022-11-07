304f247f102897a3722e2385ce1a9cdbf8e64cbd
12novel30 <79260472+12novel30@users.noreply.github.com>
2022-11-07 21:35:23 +0900

    initialize Networking project

---
+++ src/main/java/com/network/spring/Network/NetworkApplication.java
@@ -0,0 +1,14 @@
+package com.network.spring.Network;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NetworkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NetworkApplication.class, args);
+		System.out.println("fight!");
+	}
+
+}
---
+++ src/test/java/com/network/spring/Network/NetworkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.network.spring.Network;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NetworkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
